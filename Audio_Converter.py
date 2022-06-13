d39a7dc6fd04e0df494f858f37af1cdfbd198ea8
Lin-Rexter <a0989503610@gmail.com>
2022-06-13 23:54:57 +0800

    Fix bug and update english version

--- Audio_Converter.py
+++ Audio_Converter.py
@@ -36,7 +36,7 @@ class Input_ask:
         self.Input()
 
     def Input(self):
-        console.print(self.Message + ": ", end="")
+        console.print(self.Message + ": ")
         self.Reply = input().strip()
         self.Check()
 
@@ -83,6 +83,7 @@ class Input_ask:
 
                 if self.Reply == "":
                     self.Reply = os.getcwd()
+                    Target_file = ""
                     Target_file = os.path.join(self.Reply, Target_file_name)
                 if not os.path.exists(self.Reply):
                     self.Err_message = "路徑錯誤或不存在，請重新輸入正確路徑!"
@@ -142,19 +143,22 @@ def Chinese():
         Lines("[bold cyan]當前目錄下的音樂檔案[/]", "-", 74)
         try:
             global Music_list, Music_number
+            Music_list = []
             Music_number = 0
+            Music_format = ["mp3", "wav", "flac", "aac", "m4a", "wma", "ogg"]
 
-            Music_list = subprocess.getoutput(
-                "dir /B /O:S /A-d *.mp3 *.flac *.wav *.aac *.m4a *.wma *.ogg 2> nul"
-            ).split()
+            for file in os.listdir():
+                if file.split(".")[-1] in Music_format:
+                    Music_list.append(file)
+                    Music_number += 1
 
-            # 顯示當前音檔清單
+            Music_list.sort(key=lambda x: x.split(".")[-1])
+        
             table = Table(show_header=True, header_style="bold green", box=box.ROUNDED)
             table.add_column("號碼", style="cyan", justify="center")
-            table.add_column("檔案名稱", style="cyan", justify="center")
+            table.add_column("檔案名稱", style="cyan", justify="right")
             table.add_column("檔案大小", style="cyan", justify="center")
             for i in range(len(Music_list)):
-                Music_number += 1
                 table.add_row(
                     "(" + str(i + 1) + ")",
                     Music_list[i],
@@ -162,8 +166,11 @@ def Chinese():
                 )
             console.print(table)
 
-            console.print("\n[bold cyan]總共數量:{}個[/]\n".format(Music_number))
-        except Exception:
+            console.print(
+                "\n[bold cyan]總共數量:[bold #10b4eb]{}[/]個[/]\n".format(Music_number)
+            )
+        except Exception as e:
+            print(e)
             console.print("\n[bold #00ffa2]當前目錄下沒有音樂檔案![/]\n")
         Choice()
 
@@ -176,16 +183,19 @@ def Chinese():
         # Music_extension = 檔案的副檔名
         global Music_path, Music_name, Music_extension
 
+        Music_path = ""
+        Music_name = ""
+        Music_extension = ""
+
         Music_path = Input_ask(
             "\n請輸入[bold #ff7300]音檔名稱(須在當前目錄下)[/]或[bold #ff7300]完整路徑[/]或[bold #ff7300]以上檔案對應號碼[/]",
             "Check_File-Name",
             True,
         ).Reply
-
         # 判斷輸入的數值是否為代號，將數值變換為音檔名稱
         try:
             Music_name = Music_list[int(Music_path) - 1]  # 列表從0開始所以減1
-            Music_path = os.path.abspath(Music_path)
+            Music_path = os.path.abspath(Music_name)
         except:
             Music_name = os.path.basename(Music_path)
             Music_path = os.path.abspath(Music_path)
@@ -198,6 +208,7 @@ def Chinese():
     def Ask_Name():
         Lines("", "-", 70)  # 分隔線
         global Ask_name
+        Ask_name = ""
 
         Ask_name = Input_ask(
             "\n請輸入轉換後的音檔名稱[bold #ff7300](按下Enter默認原檔名)[/]", "Check_File-Format", True
@@ -215,6 +226,8 @@ def Chinese():
         # Target_audio = 選擇的音源格式
         # Target_file_name = 轉換後的檔案完整名稱
         global Target_audio, Target_file_name
+        Target_audio = ""
+        Target_file_name = ""
 
         questions = [
             {
@@ -241,6 +254,8 @@ def Chinese():
         # Ask_path = 設置轉換後的檔案放置位置
         # Target_file_path = 轉換後的檔案完整路徑
         global Ask_path, Target_file_path
+        Ask_path = ""
+        Target_file_path = ""
 
         Ask_path = Input_ask(
             "\n請輸入轉換後的音檔存放位置[bold #ff7300](按下Enter默認當前位置)[/]", "Check_File-Path", True
@@ -313,6 +328,8 @@ def Chinese():
     # 轉換處理主程序
     def ffmpeg():
         global Total_Time
+        Total_Time = 0
+
         Audio_format = {
             "mp3": '{} "{}" -q:a 0 -map_metadata 0 -id3v2_version 3 "{}"',
             "wav": '{} "{}" -c:a pcm_s16le -f wav "{}"',
@@ -324,10 +341,11 @@ def Chinese():
         }
         if Target_audio in Audio_format:
             start = time.perf_counter()
-            os.system(
+            subprocess.run(
                 Audio_format[Target_audio].format(
                     "ffmpeg -i", Music_path, Target_file_path
-                )
+                ),
+                shell=True,
             )
             end = time.perf_counter()
         Total_Time = end - start
@@ -348,119 +366,231 @@ def Chinese():
         ShowList()
 
 
-# 語言版本-English(尚未更新!!)
+# 語言版本-English
 def English():
+
     Clear()
-    # Show list of music file in the current directory
+    # Show list of audio file in current directory
     def ShowList():
-        console.print(
-            "{:-^70}".format("[bold cyan]Music files in the current directory[/]")
-        )
+        Lines("[bold cyan]The list of audio files in current directory[/]", "-", 84)
+
         try:
-            Music_list = subprocess.getoutput(
-                "dir /B /O:S /A-d *.mp3 *.flac *.wav *.aac *.m4a *.wma 2> nul"
-            ).split("\n")
+            global Music_list, Music_number
+            Music_list = []
             Music_number = 0
+            Music_format = ["mp3", "wav", "flac", "aac", "m4a", "wma", "ogg"]
+
+            for file in os.listdir():
+                if file.split(".")[-1] in Music_format:
+                    Music_list.append(file)
+                    Music_number += 1
+
+            Music_list.sort(key=lambda x: x.split(".")[-1])
+
+            table = Table(show_header=True, header_style="bold green", box=box.ROUNDED)
+            table.add_column("Number", style="cyan", justify="center")
+            table.add_column("File Name", style="cyan", justify="center")
+            table.add_column("File Size", style="cyan", justify="center")
             for i in range(len(Music_list)):
                 Music_number += 1
-                console.print(
-                    "("
-                    + str(i + 1)
-                    + ") "
-                    + Music_list[i]
-                    + "\t\tThe size of the file: "
-                    + str((Convert_size(os.path.getsize(Music_list[i]))))
+                table.add_row(
+                    "(" + str(i + 1) + ")",
+                    Music_list[i],
+                    str(Convert_size(os.path.getsize(Music_list[i]))),
                 )
+            console.print(table)
+
             console.print(
-                "\n[bold cyan]The total number of music files: {}[/]\n".format(
+                "\n[bold cyan]Total number of music files:[/] [bold #10b4eb]{}[/]\n".format(
                     Music_number
                 )
             )
         except:
             console.print(
-                "\n[bold #00ffa2]There is no music file in the current directory![/]\n"
+                "\n[bold #00ffa2]There is no audio file in the current directory![/]\n"
             )
         Choice()
 
-    # Enter the choice of the music file to be converted
+    # Enter the number of audio file to convert
     def Choice():
-        global Music_name, Music_names, Music_path
-        console.print("{:-^70}".format(""))  # 分隔線
-        Music_path = input("Please enter the file name or path and file name:").strip()
-        if os.path.isfile(Music_path):
+        Lines("", "-", 70)
+        # Music_path = audio file path
+        # Music_name = audio file name
+        # Music_extension = audio file extension
+        global Music_path, Music_name, Music_extension
+        Music_path = ""
+        Music_name = ""
+        Music_extension = ""
+
+        Music_path = Input_ask(
+            "\n[bold #ebcc36]Please enter the number of audio file to convert[/]",
+            "Check_File-Name",
+            True,
+        ).Reply
+
+        try:
+            Music_name = Music_list[
+                int(Music_path) - 1
+            ]  # Because the index of the list starts from 0, so we need to minus 1
+            Music_path = os.path.abspath(Music_name)
+        except:
             Music_name = os.path.basename(Music_path)
-            Music_names = Music_name.split(".")[0]
-        else:
-            Clear()
-            console.print(
-                "[bold red]Please enter the correct file name or path and file name![/]\n"
-            )
-            ShowList()
+            Music_path = os.path.abspath(Music_path)
+        Music_extension = Music_name.split(".")[1]
+
         console.print(
-            "\nThe file name of the music to be converted is:{}\n".format(Music_name)
+            "\nThe audio file of choice: [bold deep_sky_blue3]{}[/]\n".format(
+                Music_name
+            )
+        )
+        Ask_Name()
+
+    # Enter the converted file name
+    def Ask_Name():
+        Lines("", "-", 70)
+        global Ask_name
+        Ask_name = ""
+
+        Ask_name = Input_ask(
+            "\nPlease enter the name of the converted audio file [bold #ff7300](Press Enter to use the original file name)[/]",
+            "Check_File-Format",
+            True,
+        ).Reply
+
+        if Ask_name == "":
+            Ask_name = Music_name.split(".")[0]
+
+        console.print(
+            "\nThe name of the converted audio file: [bold deep_sky_blue3]{}[/]\n".format(
+                Ask_name
+            )
         )
         Choice_Audio()
 
-    # Enter the choice of the audio format to be converted
+    # Choose the audio format
     def Choice_Audio():
-        console.print("{:-^70}".format(""))  # 分隔線
+        Lines("", "-", 70)
+
+        # Target_audio = Select audio format
+        # Target_file_name = Full name of the file after conversion
+        global Target_audio, Target_file_name
+        Target_audio = ""
+        Target_file_name = ""
+
         questions = [
             {
                 "type": "list",
                 "name": "Choice_Audio",
-                "message": "Please select the audio format to be converted:",
+                "message": "Please select the converted audio format:",
                 "choices": ["MP3", "WAV", "AAC", "FLAC", "M4A", "OGG", "WMA"],
                 "filter": lambda val: val.lower(),
             }
         ]
-        global Target_file, Music_audio
-        Music_audio = prompt(questions, style=custom_style_1)
-        Target_file = os.path.join(
-            os.path.dirname(Music_names),
-            Music_names + "." + Music_audio["Choice_Audio"],
-        )
+        Target_audio = prompt(questions, style=custom_style_1)
+        Target_audio = Target_audio.get("Choice_Audio")
+        Target_file_name = Ask_name + "." + Target_audio
+
         console.print(
-            "\nThe file name of the converted music is:{}\n".format(Target_file)
-        )
-        console.print("{:-^70}".format(""))  # 分隔線
-        console.print(
-            "You choose to convert the {} to {} format.\n".format(
-                Music_name, Target_file
+            "\nThe converted audio file: [bold deep_sky_blue3]{}[/]\n".format(
+                Target_file_name
             )
         )
-        Ask = input("Are you sure to convert?(Y/N):")
-        if Ask.lower() == "y":
+        Ask_Path()
+
+    # Enter the path of the converted file
+    def Ask_Path():
+        Lines("", "-", 70)
+
+        # Ask_path = The audio file location after conversion
+        # Target_file_path = The full path of the converted file
+        global Ask_path, Target_file_path
+        Ask_path = ""
+        Target_file_path = ""
+
+        Ask_path = Input_ask(
+            "\nPlease enter the location of the converted audio file [bold #ff7300](Press Enter to use the current position)[/]",
+            "Check_File-Path",
+            True,
+        ).Reply
+        Ask_path = os.path.abspath(Ask_path)
+        Target_file_path = os.path.join(Ask_path, Target_file_name)
+
+        console.print(
+            "\n\nThe location of the converted audio file: [bold deep_sky_blue3]{}[/]\n".format(
+                Ask_path
+            )
+        )
+        Ask_Convert()
+
+    # Ask whether to start the conversion
+    def Ask_Convert():
+        Lines("[bold cyan]Conversion Process[/]", "-", 84)
+
+        console.print(
+            "\nThe choose file path:\n[bold deep_sky_blue3]{}[/]\n".format(
+                os.path.dirname(Music_path)
+            )
+        )
+        console.print(
+            "The Converted file path:\n[bold deep_sky_blue3]{}[/]\n".format(Ask_path)
+        )
+
+        table = Table(show_header=True, header_style="bold green", box=box.DOUBLE_EDGE)
+        table.add_column("The choose file", style="cyan", justify="center")
+        table.add_column("The converted file", style="cyan", justify="center")
+        table.add_row(Music_name, Target_file_name)
+        console.print(table)
+
+        Ask = Input_ask(
+            "\n[bold #ebcc36]Do you want to convert the file?[/]", "Check_Ask", True
+        ).Reply
+        if Ask == True:
             Audio_Convert()
-        else:
-            console.print("\nConversion canceled Please wait...\n")
+        elif Ask == False:
+            console.print("\n\n[bold #00ffa2]The conversion is cancelled![/]\n")
+            Clear()
             ShowList()
 
-    # Convert the audio format
+    # Start the conversion
     def Audio_Convert():
-        console.print("\nConversion started...\n")
-        console.print("{:-^70}".format(""))  # 分隔線
-        if Music_audio["Choice_Audio"] == Music_name.split(".")[1]:
-            console.print(
-                "\nThe converted audio format is the same as the original file, please re-select the converted audio format!\n"
-            )
-            Ask_End()
-        else:
-            start = time.perf_counter()
+        Lines("", "-", 70)
+        if Music_extension == Target_audio:
+            Ask2 = Input_ask(
+                "[bold #ebcc36]\nThe file is the same format as the target format,Do you want to convert it [Y/N] [/]",
+                "Check_Ask",
+                True,
+            ).Reply
+            if Ask2 == True:
+                print("\n")
+                pass
+            elif Ask2 == False:
+                Ask_End()
+
+        try:
+            console.print("\n[bold red]Converting...[/]\n")
             ffmpeg()
-            end = time.perf_counter()
-            Times = Decimal((end - start)).quantize(
-                Decimal("0.00"), rounding=ROUND_HALF_UP
-            )
-        console.print("\n{:-^70}".format(""))  # 分隔線
+        except Exception as e:
+            print(e)
+            console.print("\n\n[bold red]Conversion failed![/]\n")
+            sys.exit(0)
+
+        Times = Decimal((Total_Time)).quantize(Decimal("0.000"), rounding=ROUND_HALF_UP)
+
+        print("\n")
+        Lines("", "-", 70)
+
         console.print(
-            "\nConversion completed! The time is {:.2f} seconds.\n".format(Times),
-            flush=True,
-            file=sys.stderr,
+            "\nConversion completed! Processing time is:[bold #10b4eb]{} seconds[/][bold red] (Estimated time is not accurate)[/]\n".format(
+                Times
+            )
         )
         Ask_End()
 
     # Conversion processing main program
     def ffmpeg():
+        global Total_Time
+        Total_Time = 0
+
         Audio_format = {
             "mp3": '{} "{}" -q:a 0 -map_metadata 0 -id3v2_version 3 "{}"',
             "wav": '{} "{}" -c:a pcm_s16le -f wav "{}"',
@@ -471,26 +601,33 @@ def English():
             "wma": '{} "{}" -c:a wmav2 "{}"',
         }
         if Target_audio in Audio_format:
+            start = time.perf_counter()
             os.system(
-                Audio_format[Target_audio].format("ffmpeg -i", Music_path, Target_file)
+                Audio_format[Target_audio].format(
+                    "ffmpeg -i", Music_path, Target_file_path
+                )
             )
+            end = time.perf_counter()
+        Total_Time = end - start
 
     # Ask whether to continue to convert
     def Ask_End():
-        console.print("{:-^70}".format(""))  # 分隔線
-        Ask2 = input("\nDo you want to convert another file?(Y/N):")
-        if Ask2.lower() == "y":
+        Lines("", "-", 70)
+        Ask3 = Input_ask(
+            "\n[bold #ebcc36]Do you want to convert another file? [Y/N] [/]",
+            "Check_Ask",
+            True,
+        ).Reply
+        if Ask3 == True:
+            Clear()
             ShowList()
-        else:
-            console.print("\n\nConversion completed!\n")
+        elif Ask3 == False:
+            console.print("\n\n[bold #81eb36]Conversion completed![/]\n")
+            Stop()
             sys.exit()
 
     if __name__ == "__main__":
-        try:
-            ShowList()
-        except BaseException as e:
-            console.print("\n\n[bold #52c5ff]Operation cancelled![/]\n")
-            sys.exit(0)
+        ShowList()
 
 
 def Ask_language():
@@ -528,6 +665,7 @@ def Ask_language():
 
 if __name__ == "__main__":
     try:
+        #Window("full")
         Ask_language()
     except Exception:
         console.print("\n\n[bold red]已中斷程序! Operation cancelled![/]\n")
